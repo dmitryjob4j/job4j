5d0851264e9e75d7c5f992cde4518d7f05ae93d0
dmitryjob4j <haoos@inbox.ru>
2020-03-01 15:11:38 +0300

    5.0.1. Сумма чисел.[#242819]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,22 @@
+package ru.job4j.loop;
+
+public class Counter {
+    public static int sum(int start, int finish){
+        int sum = 0;
+        //указываем циклу переменные где начало где конец
+        for (int index = start; index <= finish; index++){
+            //Сумируем заданный диапозон цикла
+            sum = sum + index;
+        }
+        return sum;
+    }
+    //выводим метод sum c различными значениями
+    public static void main (String[] args){
+        System.out.println(sum(0,10));
+        System.out.println(sum(3,8));
+        System.out.println(sum(1,1));
+        System.out.println(sum(9,16));
+        System.out.println(sum(33,45));
+        System.out.println(sum(100,101));
+    }
+}
