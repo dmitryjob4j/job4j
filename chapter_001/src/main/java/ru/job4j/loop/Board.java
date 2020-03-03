a466c54fdeba898eb784d4c46d137d94167bacaf
dmitryjob4j <haoos@inbox.ru>
2020-03-03 15:38:16 +0300

    5.3. Построить шахматную доску в псевдографике.[#242825]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+public class Board {
+    public static void paint(int width, int height){
+        //Переводим на новую строку height раз (строки)
+        for (int row = 0; row < height; row++){
+            //Печатаем X - width раз (столбцы)
+            for (int cell = 0; cell < width; cell++){
+                //Проверяем условия когда печатать пробел а когда 0
+                if ((cell + row)%2 == 0) {
+                    System.out.print("X");
+                }else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    //Вызываем метод для печати шахмотной доски
+    public static void main(String[] args){
+        paint(3,3);
+        System.out.println();
+        paint(4,4);
+        System.out.println();
+        paint(8,8);
+    }
+}
