0f88a3d72a6ef6386888b8b3226391bca570ac80
dmitryjob4j <haoos@inbox.ru>
2020-03-04 20:12:47 +0300

    5.5. Простое число.[#242817]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,13 +3,11 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check (int number){
         boolean prime = true;
-        //проверяем ранво ли чесло 1 елси да то это не простое
         if (number == 1) {
          prime = false;
         }
         else {
             for (int index = 2; index < number; index++) {
-            //проверяем числа от 2 до number - 1 делимость по модулю
             if (number % index == 0) {
                 prime = false;
                 break;
--- chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -3,19 +3,16 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 public class CheckPrimeNumberTest {
-    //проверяем простое ли число 5
     @Test
     public void when5(){
         boolean rsl = CheckPrimeNumber.check(5);
         assertThat(rsl, is(true));
     }
-    //проверяем простое ли число 4
     @Test
     public void when4(){
         boolean rsl = CheckPrimeNumber.check(4);
         assertThat(rsl, is(false));
     }
-    //проверяем простое ли число 1
     @Test
     public void when1(){
         boolean rsl = CheckPrimeNumber.check(1);
