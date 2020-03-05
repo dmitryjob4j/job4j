33a3f2c4b0e4b7ee8fe07c25f7c0788221604100
dmitryjob4j <haoos@inbox.ru>
2020-03-05 13:40:57 +0300

    1_7_6.0.2. Заполнение массива.[#242758]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,9 +8,18 @@ package ru.job4j.array;
  * @since 05.03.2020
  */
 public class ArrayDefinition {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Dmitry Stepanov";
+        names[2] = "05 march 2020";
+        names[3] = "elementary";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
