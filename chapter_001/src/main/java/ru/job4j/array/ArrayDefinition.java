35c3fd8db015574bc190f9e1211fd77bf681faf3
dmitryjob4j <haoos@inbox.ru>
2020-03-05 13:27:00 +0300

    1_7_6.0.1. Объявление массива.[#242766]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+/**
+ * Class класс для для обевления пременных масива
+ *
+ * @author dstepanov
+ * @version 1
+ * @since 05.03.2020
+ */
+public class ArrayDefinition {
+    public void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
