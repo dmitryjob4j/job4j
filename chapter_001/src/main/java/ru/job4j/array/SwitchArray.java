deda55deadab17d5410979748bc89ab3c02e2b7f
dmitryjob4j <haoos@inbox.ru>
2020-03-07 20:21:27 +0300

    6.2.2. Переставить элементы массива[#242763]

--- chapter_001/src/main/java/ru/job4j/array/SwitchArray.java
+++ chapter_001/src/main/java/ru/job4j/array/SwitchArray.java
@@ -1,12 +1,48 @@
 package ru.job4j.array;
 
+/**
+ * Class переставляет элементы массива
+ *
+ * @author dstepanov
+ * @version 1.1
+ * @since 07.03.2020
+ */
 public class SwitchArray {
+    /**
+     * swap менеят местами указанные элементы
+     *
+     * @param array  - массив
+     * @param source - элемент который нужно поменяять на dest
+     * @param dest   - элемент который нужно поменять на source
+     * @return - возврощает перестановленный массив array
+     */
+    public static int[] swap(int[] array, int source, int dest) {
+        if (source >= 0 && source <= array.length - 1 && dest >= 0 && dest <= array.length - 1) {
+            int temp = array[source];
+            array[source] = array[dest];
+            array[dest] = temp;
+        }
+        return array;
+    }
+
+    /**
+     * Метод который меняет местами элементы местами
+     *
+     * @param array - масиив в котором нужно поменять первый и последний элемент местами
+     * @return - возврощает результат перестановки
+     */
     public static int[] swapBorder(int[] array) {
         int temp = array[0];
         array[0] = array[array.length - 1];
         array[array.length - 1] = temp;
         return array;
     }
+
+    /**
+     * метов который заджает массив и выводит на экран результат перестановки
+     *
+     * @param args выводит результат
+     */
 
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3, 4, 5, 6};
